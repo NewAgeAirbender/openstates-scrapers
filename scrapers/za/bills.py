e04586b492a8a273fc102c8ac1793ca487706284
NewAgeAirbender <34139325+NewAgeAirbender@users.noreply.github.com>
2023-04-06 23:56:59 -0500

    ZA: mostly working, got to detail page

---
+++ scrapers/za/bills.py
@@ -0,0 +1,114 @@
+import logging
+import json
+import requests
+from openstates.scrape import Bill, Scraper
+from spatula import JsonPage, HtmlPage, URL
+
+chamber_map = {"National Council of Provinces": "upper", "National Assembly": "lower"}
+
+
+def graphql_query(data, link):
+    return URL(
+        link,
+        timeout=30,
+        method="POST",
+        headers={
+            "Content-Type": "application/json",
+        },
+        data=json.dumps(data),
+        verify=False,
+    )
+
+
+def get_page_source(session, source_num, link):
+    return graphql_query(
+        {
+            "query": '{"keyword":null,"committee":null,"year":"' + session + '"}',
+            "bill_status": source_num,
+        },
+        link,
+    )
+
+
+class BillDetailPage(HtmlPage):
+    example_source = "https://www.parliament.gov.za/bill/2308132"
+
+    def get_source_from_input(self):
+        # site = requests.get('https://www.parliament.gov.za')
+        # headers = {"Cookie": site.cookies,
+        #            "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+        #            "Host": "parliament.gov.za",
+        #            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"}
+        return URL(self.input.sources[0]["url"], verify=False, timeout=30)
+
+    def process_page(self):
+        b = self.input
+        page = self.root
+
+        abstract = page.xpath('//*[@id="content"]/div[1]/text()')[0].strip()
+        b.add_abstract(abstract, note="Summary")
+
+        versions = page.xpath('//*[@id="versions"]/table/tbody/tr')
+        for version in versions:
+            name = version.xpath("td[2]/a/button/text()")[0]
+            link = version.xpath("td[2]/a/@href")[0]
+            date = version.xpath("td[3]/text()")[0]
+            b.add_version_link(name, link, media_type="application/pdf", date=date)
+
+        sponsor = page.xpath('//*[@id="versions"]/p/text()')[0].strip()
+        b.add_sponsorship(
+            sponsor,
+            classification="primary",
+            entity_type="person",
+            primary=True,
+        )
+        yield b
+
+
+class BillList(JsonPage):
+    def process_page(self):
+        data = self.response.json().get("bills")
+        for bill in data:
+            title = bill["name"]
+            bill_id = bill["bill_no_number"]
+            session = bill["bill_no_year"]
+            # date = bill["introduced_date"]
+            link_num = bill["id"]
+            chamber = "lower" if bill["bill_status"][1] == "A" else "upper"
+
+            b = Bill(bill_id, session, title, chamber=chamber, classification="bill")
+            link = f"https://www.parliament.gov.za/bill/{link_num}"
+            b.add_source(link)
+            yield BillDetailPage(b)
+
+
+class Assembly(BillList):
+    # def get_source_from_input(self):
+    #     return get_page_source('2023', 1, 'https://www.parliament.gov.za/bills')
+    source = get_page_source("2023", 1, "https://www.parliament.gov.za/bills")
+
+
+class Council(BillList):
+    source = get_page_source("2023", 2, "https://www.parliament.gov.za/bills")
+
+
+class Sent(BillList):
+    source = get_page_source("2023", 6, "https://www.parliament.gov.za/bills-passed")
+
+
+class Passed(BillList):
+    source = get_page_source("2023", 7, "https://www.parliament.gov.za/bills-passed")
+
+
+class ZABillScraper(Scraper):
+    def scrape(self, session="2023"):
+        self.raise_errors = False
+        self.retry_attempts = 1
+        self.retry_wait_seconds = 3
+        requests.Session()
+        requests.Session.cookies.set_cookie()
+
+        # spatula's logging is better than scrapelib's
+        logging.getLogger("scrapelib").setLevel(logging.WARNING)
+        bill_list = Assembly({"session": session})
+        yield from bill_list.do_scrape()
